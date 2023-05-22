8cef55b1d1d73a319ab1682431d999e89d786bcf
kendyiand <hellmax2206@gmail.com>
2023-05-22 21:45:06 +0300

    create plugin with block break

--- src/main/java/com/eneskey/paper/myplug/MyPlug.java
+++ src/main/java/com/eneskey/paper/myplug/MyPlug.java
@@ -1,39 +1,82 @@
 package com.eneskey.paper.myplug;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import com.eneskey.paper.myplug.handler.NewEventHandler;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
+import org.bukkit.scheduler.BukkitRunnable;
 
-
-
-public final class MyPlug extends JavaPlugin {
-
+public final class MyPlug extends JavaPlugin implements Listener {
+    private WorldGuardPlugin worldGuard;
     @Override
     public void onEnable() {
-        getCommand("plug").setExecutor(new CommandExecutor() {
-            @Override
-            public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        getServer().getPluginManager().registerEvents(this, this);
 
-                if (strings.length == 0) {
-                    commandSender.sendMessage("Reload plugin: /plug reload");
-                    return true;
-                }
-
-                if (strings[0].equalsIgnoreCase("reload")) {
-                    reloadConfig();
-                    commandSender.sendMessage("Plugin reloaded");
-                    return true;
-                }
-
-                return true;
-            }
-        });
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+
+    @EventHandler
+    public void handleBlockBreakEvent(BlockBreakEvent event) {
+        Block block = event.getBlock();
+        Material blockType = block.getType();
+
+        if (blockType == Material.COAL_ORE) {
+            event.setCancelled(true);
+            event.getBlock().setType(Material.COBBLESTONE);
+            ItemStack coal = new ItemStack(Material.COAL, 1);
+            Location blockLocation = event.getBlock().getLocation();
+            event.getBlock().getWorld().dropItemNaturally(blockLocation, coal);
+
+            BukkitRunnable task = new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if (block.getType() == Material.COBBLESTONE) {
+                        block.setType(Material.COAL_ORE);
+                    }
+                }
+            };
+            task.runTaskLater(this, 20);
+        }
+        else if (blockType == Material.COBBLESTONE) {
+            event.setCancelled(true);
+            event.getBlock().setType(Material.BEDROCK);
+            ItemStack cobblestone = new ItemStack(Material.COBBLESTONE, 1);
+            Location blockLocation = event.getBlock().getLocation();
+            event.getBlock().getWorld().dropItemNaturally(blockLocation, cobblestone);
+
+            BukkitRunnable task = new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if (block.getType() == Material.BEDROCK) {
+                        block.setType(Material.COAL_ORE);
+                    }
+                }
+            };
+            task.runTaskLater(this, 40);
+        }
+    }
+    private boolean isInSpawnRegion(Location location) {
+        if (worldGuard == null) {
+            return false;
+        }
+
+        World world = location.getWorld();
+        ApplicableRegionSet regionSet = worldGuard.getRegionManager(world).getApplicableRegions(location);
+
+        for (ProtectedRegion region : regionSet) {
+            if (region.getId().equalsIgnoreCase("spawn")) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
